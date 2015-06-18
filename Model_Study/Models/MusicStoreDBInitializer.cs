7c1b43518ab457926b6da0d34c952e7de0d3c0cd
邱满 <qiumanlove@msn.cn>
2015-06-19 07:55:38 +0800

    修改 未成功

--- Model_Study/Models/MusicStoreDBInitializer.cs
+++ Model_Study/Models/MusicStoreDBInitializer.cs
@@ -5,16 +5,20 @@ using System.Web;
 
 namespace Model_Study.Models
 {
-    public class MusicStoreDBInitializer : System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDBContext>
+    public class MusicStoreDBInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MusicStoreDBContext>
     {
         protected override void Seed(MusicStoreDBContext context)
         {
+            base.Seed(context);
+
             context.Artists.Add(new Artist { Name = "周杰伦" });
+            context.SaveChanges();
             //context.Artists.Add(new Artist { Name = "Beyond" });
             context.Genres.Add(new Genre { Name = "中国风" });
+            context.SaveChanges();
             //context.Genres.Add(new Genre { Name = "Rock" });
             context.Albums.Add(new Album { Artist = new Artist { Name = "陈奕迅" }, Genre = new Genre { Name = "R&P" }, Price = 1200, Title = "Caravan" });
-            base.Seed(context);
+            context.SaveChanges();
         }
     }
 }
